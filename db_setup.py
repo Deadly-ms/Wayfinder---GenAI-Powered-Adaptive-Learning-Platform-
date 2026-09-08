ea9f823d2a250a65316b61c2e2776651710f1615
Deadly-ms <pearlzone143@gmail.com>
2026-09-09 03:07:45 +0530

    Fix Render deploy and user signup schema

--- db_setup.py
+++ db_setup.py
@@ -6,9 +6,9 @@ db = SQLAlchemy()
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(150), unique=True, nullable=False)
-    email = db.Column(db.String(150), unique=True, nullable=False)
-    password = db.Column(db.String(150), nullable=False)
+    username = db.Column(db.String(255), unique=True, nullable=False)
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
     interests = db.Column(db.String(500))
     current_skills = db.Column(db.String(500))
     learning_style = db.Column(db.String(50))
